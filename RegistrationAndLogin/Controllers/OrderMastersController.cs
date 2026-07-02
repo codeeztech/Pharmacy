893928d0a065cb8918e246e52ef2d4cb68211023
Muhammad Shahroz Khan <41835928+codeeztech@users.noreply.github.com>
2026-07-02 17:17:04 +0400

    [CorpAI] Fix issue #9: Order search by Order ID returns ALL orders when a non-numer

--- RegistrationAndLogin/Controllers/OrderMastersController.cs
+++ RegistrationAndLogin/Controllers/OrderMastersController.cs
@@ -25,9 +25,11 @@ namespace RegistrationAndLogin.Controllers
 
                 if (SearchBy == "OrderID")
                 {
-
-                    int Id = Convert.ToInt32(SearchValue);
-                    return View(db.OrderMasters.Where(x => x.OrderID == Id || SearchValue == null).ToList());
+                    if (string.IsNullOrEmpty(SearchValue) || !int.TryParse(SearchValue, out int Id))
+                    {
+                        return View(new List<OrderMaster>());
+                    }
+                    return View(db.OrderMasters.Where(x => x.OrderID == Id).ToList());
                 }
 
                 else if (SearchBy == "Name")
